a4552a2300527d5bf4688d6cc21860e42a73150b
LiBigTree <13289771760@163.com>
2020-01-28 23:13:11 +0800

    2020/1/27 详细地字符串

--- sequence1.py
+++ sequence1.py
@@ -1,6 +1,21 @@
+# 写在字符串之前的话
+# 数、字符等数据元素 >> 元组、列表、字符串等数据结构 >> 循环、选择语句 >> 模块
+# 在我看来， 每一种编程语言对应的是其解决问题的思维。或者说，分解问题。
+# so，一学基础；二悟思想。
 
+# 字符串基本操作 2020/1/28
 
-# 字符串基本操作
+# 1、 字符串转义
+print("let's go!")
+print('let\'s go!')
+
+# 2、字符串拼接
+x = "hello, "
+y = "world!"
+print(x+y)
+
+# 3、字符串表示 str 和 repr
+
 
 '''months = [
     'January',
@@ -35,14 +50,12 @@ day_end = endings[day_number-1]
 print(month_name + ' ' + day + day_end + ', ' + year)
 '''
 
-url = input('please enter the url:')
-
-# https://www.github.com
-domain = url[12:-4]
-
-print("Domain name :" + domain)
-
-
+# url = input('please enter the url:')
+#
+# # https://www.github.com
+# domain = url[12:-4]
+#
+# print("Domain name :" + domain)
 
 
 
