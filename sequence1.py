fe6b9213aa86d155157732ddc8c622c4ad1847fb
LiBigTree <13289771760@163.com>
2020-01-30 22:18:54 +0800

    2020/1/30 添加了一些字符串的内容

--- sequence1.py
+++ sequence1.py
@@ -1,54 +1,38 @@
 # 写在字符串之前的话
 # 数、字符等数据元素 >> 元组、列表、字符串等数据结构 >> 循环、选择语句 >> 模块
-# 在我看来， 每一种编程语言对应的是其解决问题的思维。或者说，分解问题。
+# 在我看来， 每一种编程语言对应的是其解决问题的思维。或者说，分解问题的思维。
 # so，一学基础；二悟思想。
 
 # 字符串基本操作 2020/1/28
 
 # 1、 字符串转义
+print(1)
 print("let's go!")
 print('let\'s go!')
 
 # 2、字符串拼接
+print(2)
 x = "hello, "
 y = "world!"
 print(x+y)
 
 # 3、字符串表示 str 和 repr
+# 直接打印字符串和print显示不一样， 所以才有上述两种说法
+print(3)
+print(repr("hello,\nworld!"))
+print(str("hello, \nworld!"))
+# >> str 会转换，显示的是用户接受的； repr显示字符串
 
+# 4、 转义字符
+print(4)
+print(r'C:\nowhere')
+# 以反斜杠结尾的字符串另作处理，如下：
+print(r'C:\Program Files''\\')
 
-'''months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-]
-
-# 月份的结尾设置
-endings = ['st', 'nd', 'rd'] + 17*['th'] \
-        + ['st', 'nd', 'rd'] + 7*['th'] \
-        + ['st']
-year = input('Year:')
-month = input('month(1-12):')
-day = input('day(1-31):')
-
-month_number = int(month)
-month_name = months[month_number - 1]
-
-day_number = int(day)
-day_end = endings[day_number-1]
-
-# January 12th, 2020
-print(month_name + ' ' + day + day_end + ', ' + year)
-'''
+# 5、 长字符串： 三个单引号 ‘\’ 用来连接行和行
+print(5)
+print('''hello, \
+world!''')
 
 # url = input('please enter the url:')
 #
