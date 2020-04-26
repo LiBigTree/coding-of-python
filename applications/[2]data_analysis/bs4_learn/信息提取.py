46cd1e505aec30071c3a04759cc50439893aa6cb
LiBigTree <13289771760@163.com>
2020-04-26 16:15:46 +0800

    update

---
+++ applications/[2]data_analysis/bs4_learn/信息提取.py
@@ -0,0 +1,53 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+def crawl_data(url):
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        # print('1:', r.status_code)
+        # print('2:', r.encoding)
+        result = r.text
+        return result
+    except:
+        print("爬取失败")
+
+
+if __name__ == '__main__':
+    # 网页爬取
+    url = 'http://python123.io/ws/demo.html'
+    demo = crawl_data(url)
+
+    soup = BeautifulSoup(demo, "html.parser")
+    # 查找所有链接
+    for link in soup.find_all('a'):
+        print(link.get('href'))
+    # .find_all(name, attrs, recursive, string, **kwargs)
+    # 返回一个列表类型，储存查找的结果
+    # name:对应标签名称的检索字符串
+    # print(soup.find_all(['a', 'b']))
+    # -True可以将显示所有标签
+    # print(soup.find_all(True))
+    # -显示以b开头的内容
+    for tag in soup.find_all(re.compile('b')):
+        print(tag.name)
+
+    # attrs: 对标签属性值的检索字符串，可标注属性检索
+    print('查找p标签中包含course属性的内容：{}'.format(soup.find_all('p', 'course')))
+    print('指定id属性的查找：{}'.format(soup.find_all(id='link1')))
+
+    # recursive:是否对子孙全部检索，默认True
+    print(soup.find_all('a', recursive=False))
+
+    # string: 对标签中字符串区域的检索字符串
+    print(soup(string="Basic Python"))  # 简化写法
+    # 单纯使用find_all搜索需要精准信息，若要实现模糊搜索，则需要正则表达式库
+    print(soup.find_all(string=re.compile("python")))
+
+    # 由于find_all函数非常常用，有如下等价：
+    # <tag>(..) = <tag>.find_all(..)
+    # soup(..) = soup.find_all(..)
+    # find_all 有七个拓展方法
---
+++ applications/[2]data_analysis/bs4_learn/大学排名爬虫实例.py
@@ -0,0 +1,41 @@
+# http://www.zuihaodaxue.com/zuihaodaxuepaiming2019.html
+import requests
+from bs4 import BeautifulSoup
+import bs4
+
+
+def crawl_data(url):
+    try:
+        r = requests.get(url, timeout=30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        # print('1:', r.status_code)
+        # print('2:', r.encoding)
+        return r.text
+    except:
+        return ''
+
+
+def get_data(u_list, html):
+    soup = BeautifulSoup(html, 'html.parser')
+    for tr in soup.find('tbody').children:
+        if isinstance(tr, bs4.element.Tag):
+            tds = tr('td')
+            u_list.append([tds[0].string, tds[1].string, tds[2].string])
+
+
+def output_data(u_list, num):
+    tplt = "{0:^10}\t{1:{3}^10}\t{2:^10}\t"  # {3}啥意思呢
+    print(tplt.format("排名", "学校名称", "总分", chr(12288)))  # 12288 表示中文字符的空格
+    for i in range(num):
+        u = u_list[i]
+        print(tplt.format(u[0], u[1], u[2], chr(12288)))
+    # print(u_list)
+
+if __name__ == '__main__':
+    url = "http://www.zuihaodaxue.com/zuihaodaxuepaiming2019.html"
+    u_info = []
+    html = crawl_data(url)
+    get_data(u_info, html)
+    output_data(u_info, 20)
+
