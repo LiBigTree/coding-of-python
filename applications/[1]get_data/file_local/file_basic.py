18ad76ba891f3db10723161a1b57e76649c18376
LiBigTree <lider7214@163.com>
2020-04-28 22:39:33 +0800

    update

--- applications/[1]get_data/file_local/file_basic.py
+++ applications/[1]get_data/file_local/file_basic.py
@@ -1,10 +1,15 @@
 # 文件基础操作的练习
 # ----open函数----
-# 1、新文件里写入内容
+# open(file, mode='r', buffering=None, encoding=None, errors=None, newline=None, closefd=True)
+# help(open)可以查看用法或者直接在pycharm中查看
+# file：文件路径；mode：文件打开模式，默认为r；buffering：缓冲，默认-1，0代表不缓冲
+# 返回文件对象
 with open('test.txt', 'r') as f:
     # r = f.write('hello world!')  # write方法返回写入的字符的个数
     # print(r)
     content = f.read()  # read方法返回以字符串形式文件里的内容
     print(content)
+
+
 
 
